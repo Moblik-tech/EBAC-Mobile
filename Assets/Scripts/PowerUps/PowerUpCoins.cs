6a89d9dca0be272113f1bed91243023dddcca46a
Moblik-tech <kaike.dias.gabani@gmail.com>
2026-06-23 22:39:11 -0300

    Ajuste no script do coletor de moedas

--- Assets/Scripts/PowerUps/PowerUpCoins.cs
+++ Assets/Scripts/PowerUps/PowerUpCoins.cs
@@ -8,12 +8,14 @@ public class PowerUpCoins : PowerUpBase
     protected override void StartPowerUp()
     {
         base.StartPowerUp();
+        PlayerController.Instance.SetPowerUpText("Coin Collector");
         PlayerController.Instance.ChangeCoinCollectorSize(sizeAmount);
     }
 
     protected override void EndPowerUp()
     {
         base.EndPowerUp();
+        PlayerController.Instance.SetPowerUpText("");
         PlayerController.Instance.ChangeCoinCollectorSize(1f);
     }
 }
